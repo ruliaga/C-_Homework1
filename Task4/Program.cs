0e9a399494e9475802660c6f2321e657b90a22c6
[Ruliaga] <ruliaga@mail.ru>
2022-08-21 23:41:54 +0500

    Task4 done

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите число A: ");
+int numberA=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число C: ");
+int numberC=int.Parse(Console.ReadLine());
+int max=numberA;
+if (numberB>max){max=numberB;}
+if (numberC>max){max=numberC;}
+Console.WriteLine($"{numberA} {numberB} {numberC} -> {max}");
