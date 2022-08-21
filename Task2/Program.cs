786050a80463e27acdab37809e9545dda8874e32
[Ruliaga] <ruliaga@mail.ru>
2022-08-21 23:24:50 +0500

    Task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число A: ");
+int numberA=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB=int.Parse(Console.ReadLine());
+if (numberA>numberB)
+{
+    Console.WriteLine($"А={numberA} B={numberB} Max={numberA}");
+}
+else
+{
+Console.WriteLine($"А={numberA} B={numberB} Max={numberB}");
+}
